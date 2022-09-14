da693272cd6f5b5e3184337fae520e0cd1b99372
AdnanOlarmmi <olarmmi@gmail.com>
2022-09-14 21:49:57 +0100

    done with the dynamics

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
 const hamburgerEl = document.querySelector('.burger');
 const XIconEl = document.querySelector('.XIcon');
 const mainNavListEl = document.querySelector('.main-nav__list');
+const featuredSpeakers = document.querySelector('.featured-speakers__items');
+
+const featuredSpeakersArray = [
+  {
+    name: 'Ali Morshedlou',
+    image: 'images/speakers/ali-morshedlou.jpg',
+    qualification: 'BEng. Computer Science',
+    about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium commodi beatae',
+  },
+  {
+    name: 'Christina Wocinte',
+    image: 'images/speakers/christina-wocintechchat.jpg',
+    qualification: 'BEng. Civil Engineering',
+    about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium commodi beatae',
+  },
+  {
+    name: 'David Mumma',
+    image: 'images/speakers/david-mumma.jpg',
+    qualification: 'BEng. Mechanical Engineering',
+    about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium commodi beatae',
+  },
+  {
+    name: 'Elin Tabitha',
+    image: 'images/speakers/elin-tabitha.jpg',
+    qualification: 'BEng. Industrial Chemistry',
+    about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium commodi beatae',
+  },
+  {
+    name: 'Gift Habeshaw',
+    image: 'images/speakers/gift-habeshaw.jpg',
+    qualification: 'BEng. Food Engineering',
+    about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium commodi beatae',
+  },
+  {
+    name: 'Juan Enclada',
+    image: 'images/speakers/juan-encalada.jpg',
+    qualification: 'BEng. Electrical Engineering',
+    about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium commodi beatae',
+  },
+];
 
 document.querySelector('.card__title').style.backgroundClip = 'text';
 
@@ -14,4 +54,27 @@ const toggleNav = () => {
   });
 };
 
+const renderFeaturedSpeaker = () => {
+  let markup = '';
+  featuredSpeakersArray.forEach((elem) => {
+    markup += `<li class="featured-speakers__item flex-row">
+    <div class="featured-speaker__image">
+      <img src="${elem.image}" alt="" />
+    </div>
+    <div class="featured-speaker flex-column">
+      <h5 class="featured-speaker__name">${elem.name}</h5>
+      <h6 class="feature-speaker__qualification">
+      ${elem.qualification}
+      </h6>
+      <p class="featured-speaker__about">
+        <span>${elem.about.substring(0, 5)}</span>${elem.about.substring(5)}
+      </p>
+    </div>
+  </li>`;
+  });
+
+  featuredSpeakers.innerHTML = markup;
+};
+
 toggleNav();
+renderFeaturedSpeaker();
