571923a34c430e9af208887aa2a9c20755714f3c
AdnanOlarmmi <olarmmi@gmail.com>
2022-09-17 22:20:46 +0100

    Make the about page semantic likewise

--- app.js
+++ app.js
@@ -9,36 +9,42 @@ const featuredSpeakersArray = [
     image: 'images/speakers/ali-morshedlou.jpg',
     qualification: 'BEng. Computer Science',
     about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium commodi beatae',
+    alt: 'Image of speaker Ali Morshedlou',
   },
   {
     name: 'Christina Wocinte',
     image: 'images/speakers/christina-wocintechchat.jpg',
     qualification: 'BEng. Civil Engineering',
     about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium commodi beatae',
+    alt: 'Image of speaker Christina Wocinte',
   },
   {
     name: 'David Mumma',
     image: 'images/speakers/david-mumma.jpg',
     qualification: 'BEng. Mechanical Engineering',
     about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium commodi beatae',
+    alt: 'Image of speaker David Mumma',
   },
   {
     name: 'Elin Tabitha',
     image: 'images/speakers/elin-tabitha.jpg',
     qualification: 'BEng. Industrial Chemistry',
     about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium commodi beatae',
+    alt: 'Image of speaker Elin Tabitha',
   },
   {
     name: 'Gift Habeshaw',
     image: 'images/speakers/gift-habeshaw.jpg',
     qualification: 'BEng. Food Engineering',
     about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium commodi beatae',
+    alt: 'Image of speaker Gift Habeshaw',
   },
   {
     name: 'Juan Enclada',
     image: 'images/speakers/juan-encalada.jpg',
     qualification: 'BEng. Electrical Engineering',
     about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium commodi beatae',
+    alt: 'Image of speaker Juan Enclada',
   },
 ];
 
@@ -61,7 +67,7 @@ const renderFeaturedSpeaker = () => {
   featuredSpeakersArray.forEach((elem) => {
     markup += `<div class="featured-speakers__item flex-row">
     <div class="featured-speaker__image">
-      <img src="${elem.image}" alt="" />
+      <img src="${elem.image}" alt="${elem.alt}" />
     </div>
     <div class="featured-speaker flex-column">
       <h5 class="featured-speaker__name">${elem.name}</h5>
